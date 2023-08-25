ea06a49209cfcc38aef8e8526ed970a2a0677aba
lsj <dltmd310@gmail.com>
2023-08-25 20:53:17 +0900

    noticeDetail

---
+++ semi_5makase/src/main/java/com/semi_5makase/notice/controller/NoticeDetailController.java
@@ -0,0 +1,60 @@
+package com.semi_5makase.notice.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.semi_5makase.notice.model.service.NoticeService;
+import com.semi_5makase.notice.model.vo.Notice;
+
+/**
+ * Servlet implementation class NoticeDetailController
+ */
+@WebServlet("/detail.no")
+public class NoticeDetailController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeDetailController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int noticeNo = Integer.parseInt(request.getParameter("num"));
+		
+		int result = new NoticeService().increaseNoticeViews(noticeNo);
+		
+		
+		if(result > 0) {
+			Notice n = new NoticeService().selectNotice(noticeNo);
+			request.setAttribute("n", n);
+			
+			
+			request.getRequestDispatcher("views/notice/noticeDetailView.jsp").forward(request, response);
+			
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
