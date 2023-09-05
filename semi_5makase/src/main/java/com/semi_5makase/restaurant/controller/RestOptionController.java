f7feb0e4d39e21ba8cc4ef0e3d983b3f06ae5254
양관우 <kidsgk4@naver.com>
2023-09-05 11:48:22 +0900

    관우커밋

---
+++ semi_5makase/src/main/java/com/semi_5makase/restaurant/controller/RestOptionController.java
@@ -0,0 +1,65 @@
+package com.semi_5makase.restaurant.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.semi_5makase.common.model.vo.Attachment;
+import com.semi_5makase.restaurant.model.service.RestaurantService;
+import com.semi_5makase.restaurant.model.vo.Restaurant;
+
+/**
+ * Servlet implementation class RestOptionController
+ */
+@WebServlet("/restOption.do")
+public class RestOptionController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RestOptionController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String searchVal = request.getParameter("searchVal");
+		int ageVal = Integer.parseInt(request.getParameter("ageVal"));
+		int ageVal2 = ageVal+9;
+		int categoryVal = Integer.parseInt(request.getParameter("categoryVal"));
+		String locationVal = request.getParameter("locationVal");
+		int tvVal = Integer.parseInt(request.getParameter("tvVal"));
+		
+		
+		ArrayList<Restaurant> optionList = new RestaurantService().selectOptionList(searchVal,ageVal,ageVal2,categoryVal,locationVal,tvVal);
+		ArrayList<Attachment> atList = new RestaurantService().selectRestAttachment();
+		ArrayList totalList = new ArrayList();
+		totalList.add(optionList);
+		totalList.add(atList);
+		
+		System.out.println("옵션리스트임!!!!!!!!!!!!!! " + optionList);
+		System.out.println("옵션at리스트임!!!!!!!!!!!!!! " + atList);
+		response.setContentType("application/json; charset=utf-8");
+		new Gson().toJson(totalList,response.getWriter());
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ semi_5makase/src/main/java/com/semi_5makase/restaurant/controller/RestTvOptionController.java
@@ -0,0 +1,68 @@
+package com.semi_5makase.restaurant.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.semi_5makase.common.model.vo.Attachment;
+import com.semi_5makase.restaurant.model.service.RestaurantService;
+import com.semi_5makase.restaurant.model.vo.Restaurant;
+
+/**
+ * Servlet implementation class RestTvOptionController
+ */
+@WebServlet("/restTvOption.do")
+public class RestTvOptionController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RestTvOptionController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int categoryVal = Integer.parseInt(request.getParameter("category"));
+		ArrayList<Restaurant> categoryList= new RestaurantService().selectCategory(categoryVal);
+		
+		ArrayList<Attachment> atList = new RestaurantService().selectRestAttachment();
+		
+//		request.setAttribute("atList", atList);
+		
+		ArrayList totalList = new ArrayList<>();
+		totalList.add(categoryList);
+		totalList.add(atList);
+
+		
+		response.setContentType("application/json; charset=utf-8");
+		new Gson().toJson(totalList, response.getWriter());
+
+//		JsonObject responseJson = new JsonObject();
+//		responseJson.add("categoryList", new Gson().toJsonTree(categoryList));
+//		responseJson.add("atList", new Gson().toJsonTree(atList));
+//		response.getWriter().write(responseJson.toString()); 이렇게하면 안됨ㅋ
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
